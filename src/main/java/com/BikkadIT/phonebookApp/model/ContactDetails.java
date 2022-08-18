0f09ac66b91a4a4f6ef637fae97729c971a2d8dc
SATYAJEET SHARDUL <aparnaschau1623@gamil.com>
2022-08-18 08:24:00 +0530

    added entity details n creates table in database

--- src/main/java/com/BikkadIT/phonebookApp/model/ContactDetails.java
+++ src/main/java/com/BikkadIT/phonebookApp/model/ContactDetails.java
@@ -1,5 +1,70 @@
 package com.BikkadIT.phonebookApp.model;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class ContactDetails {
 
+	@Id
+	public Integer contactId;
+	public String contactName;
+	public String contactNumber;
+	public String contactEmail;
+	public Character activeSwitch;
+	public LocalDate createdDate;
+	public LocalDate  updateDate;
+	public Integer getContactId() {
+		return contactId;
+	}
+	public void setContactId(Integer contactId) {
+		this.contactId = contactId;
+	}
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public String getContactEmail() {
+		return contactEmail;
+	}
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+	public Character getActiveSwitch() {
+		return activeSwitch;
+	}
+	public void setActiveSwitch(Character activeSwitch) {
+		this.activeSwitch = activeSwitch;
+	}
+	public LocalDate getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(LocalDate createdDate) {
+		this.createdDate = createdDate;
+	}
+	public LocalDate getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(LocalDate updateDate) {
+		this.updateDate = updateDate;
+	}
+	@Override
+	public String toString() {
+		return "ContactDetails [contactId=" + contactId + ", contactName=" + contactName + ", contactNumber="
+				+ contactNumber + ", contactEmail=" + contactEmail + ", activeSwitch=" + activeSwitch + ", createdDate="
+				+ createdDate + ", updateDate=" + updateDate + "]";
+	}
+	
+	
+	
 }
